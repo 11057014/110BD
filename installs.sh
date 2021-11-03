8a793b841a7936a5c7a37536b7fed41efbc743e1
11057014 <91753502+11057014@users.noreply.github.com>
2021-11-03 19:46:20 +0800

    use python

--- installs.sh
+++ installs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt install -y build-essential libssl-dev libffi-dev python3-dev python3-pip
-pip install obspy
+pip install obspy pymysql
 wget https://dev.mysql.com/get/mysql-apt-config_0.8.15-1_all.deb
 sudo dpkg -i mysql-apt-config_0.8.15-1_all.deb
 sudo apt update
