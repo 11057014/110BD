946627302426d0ab7d9713dc93ba509d94786dca
11057014 <91753502+11057014@users.noreply.github.com>
2021-11-03 23:56:07 +0800

    Update mysql_test2.py

--- mysql_test2.py
+++ mysql_test2.py
@@ -154,9 +154,10 @@ import os
 def insertTest(times,data,table):
     pool = Pool(4)
     for i in range(times):
-        print(i)
         pool.map_async(insert2mysql,[table,data])
         #insert2mysql(table,data)
+    pool.close()
+    pool.join()
     return
     
 @timeSpent
@@ -192,11 +193,11 @@ def selectGroupByStationQuery(table, network='SMT'):
 db, cursor=connect2mysql('127.0.0.1','root','0','test') #連線
 
 caseInfo = ['10萬筆資料','8萬筆資料','6萬筆資料','4萬筆資料','2萬筆資料']
-databaseCollection = [{'database':'test','collection':'test1', 'Loops':2711},
-                      {'database':'test','collection':'test2', 'Loops':2211},
-                      {'database':'test','collection':'test3', 'Loops':1611},
-                      {'database':'test','collection':'test4', 'Loops':1111},
-                      {'database':'test','collection':'test5', 'Loops':511}
+databaseCollection = [{'database':'test','collection':'test1', 'Loops':271},
+                      {'database':'test','collection':'test2', 'Loops':221},
+                      {'database':'test','collection':'test3', 'Loops':161},
+                      {'database':'test','collection':'test4', 'Loops':111},
+                      {'database':'test','collection':'test5', 'Loops':51}
                      ]
 
 for i in range(5):
