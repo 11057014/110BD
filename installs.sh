371a12731da2893781e76e954fab77ea57bbef8b
11057014 <91753502+11057014@users.noreply.github.com>
2021-11-03 18:40:08 +0800

    Update installs.sh

--- installs.sh
+++ installs.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 sudo apt install -y build-essential libssl-dev libffi-dev python3-dev python3-pip
+pip install obspy
+wget https://dev.mysql.com/get/mysql-apt-config_0.8.15-1_all.deb
+sudo dpkg -i mysql-apt-config_0.8.15-1_all.deb
+sudo apt update
+sudo apt install mysql-server mysql-client
