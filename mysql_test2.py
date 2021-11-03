09d5cf05d5feab1f6bce797532a89b45178b84ed
11057014 <91753502+11057014@users.noreply.github.com>
2021-11-03 23:40:22 +0800

    Update mysql_test2.py

--- mysql_test2.py
+++ mysql_test2.py
@@ -191,11 +191,11 @@ def selectGroupByStationQuery(table, network='SMT'):
 db, cursor=connect2mysql('127.0.0.1','root','0','test') #連線
 
 caseInfo = ['10萬筆資料','8萬筆資料','6萬筆資料','4萬筆資料','2萬筆資料']
-databaseCollection = [{'database':'test','collection':'test1', 'Loops':27},
-                      {'database':'test','collection':'test2', 'Loops':22},
-                      {'database':'test','collection':'test3', 'Loops':16},
-                      {'database':'test','collection':'test4', 'Loops':11},
-                      {'database':'test','collection':'test5', 'Loops':5}
+databaseCollection = [{'database':'test','collection':'test1', 'Loops':271},
+                      {'database':'test','collection':'test2', 'Loops':221},
+                      {'database':'test','collection':'test3', 'Loops':161},
+                      {'database':'test','collection':'test4', 'Loops':111},
+                      {'database':'test','collection':'test5', 'Loops':51}
                      ]
 
 for i in range(5):
